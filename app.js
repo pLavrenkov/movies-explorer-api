6282af88da50ed8c02e53ce9d116ee5c1f06470f
Pavel Lavrenkov <beasty2006@yandex.ru>
2022-07-17 20:13:09 +0300

    Diploma server starts

---
+++ app.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+});
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+app.listen(PORT);
+console.log(PORT);
